9f0be124b5209d0a1d6e738305f97544509a9462
Alexander Muratov <muratoffalex@gmail.com>
2024-11-24 17:16:40 +0500

    feat: add pack function to fish

---
+++ dot_config/private_fish/custom_functions.d/pack.fish
@@ -0,0 +1,10 @@
+function pack --description "Archive directory while excluding common dependency directories" --argument-names src dest
+    # Common directories/files to exclude from archiving
+    set --local excludes \
+        --exclude='./vendor' \
+        --exclude='./node_modules' \
+        --exclude='.DS_Store' \
+        --exclude='./Pods' \
+        --exclude='./.cache'
+    tar $excludes -czf "$dest.tar.gz" "$src"
+end
