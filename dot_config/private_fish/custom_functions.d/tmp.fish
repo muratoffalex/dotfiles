e0eff84e1812a2cff4979002c733cc9f9104f700
Alexander Muratov <muratoffalex@gmail.com>
2025-02-23 18:05:11 +0500

    feat(fish.tmp): if arg is git link - clone

--- dot_config/private_fish/custom_functions.d/tmp.fish
+++ dot_config/private_fish/custom_functions.d/tmp.fish
@@ -1,9 +1,18 @@
 function tmp --argument name --description "Create directory in ~/tmp and run tmux session inside"
     set -l directory_path ~/tmp
-    if test -n "$name"
+
+    if string match -q -- "*.git" "$name"; or string match -q -- "git@*" "$name"
+        set repo_name (basename $name .git)
+        set directory_path ~/tmp/$repo_name
+
+        git clone $name $directory_path
+    else if test -n "$name"
         set directory_path ~/tmp/$name
+        mkdir -p $directory_path
+    else
+        mkdir -p $directory_path
     end
-    mkdir -p $directory_path
+
     cd $directory_path
     sesh connect $directory_path
 end
