7d15822fdd8d4fe8993cf3a009f302d2c05f1ae4
Alexander Muratov <muratoffalex@gmail.com>
2025-09-10 12:14:37 +0500

    refactor(home-manager): migrate zen browser config to own file, use beta instead twilight

---
+++ nixos/modules/home/programs/zen.nix
@@ -0,0 +1,27 @@
+{ pkgs, inputs, ... }:
+{
+  imports = [ inputs.zen-browser.homeModules.beta ];
+
+  programs.zen-browser = {
+    enable = true;
+    nativeMessagingHosts = [ pkgs.tridactyl-native ];
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+  };
+}
