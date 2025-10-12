685ec214587ec088d4576a2e59d5e192e657e40f
Alexander Muratov <muratoffalex@gmail.com>
2025-10-12 11:40:45 +0500

    feat(nixos): Add nix-on-droid configuration

---
+++ nixos/hosts/nix-on-droid/default.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.etcBackupExtension = ".bak";
+  system.stateVersion = "24.05";
+  nixpkgs.config.allowUnfree = true;
+  console = {
+    keyMap = "colemak";
+  };
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  time.timeZone = "Asia/Yekaterinburg";
+
+  environment.packages = with pkgs; [
+    vim
+  ];
+
+  home-manager = {
+    backupFileExtension = "hm-bak";
+    useGlobalPkgs = true;
+
+    config =
+      { config, lib, pkgs, ... }:
+      {
+        imports = [
+          ../../modules/home/base.nix
+          ../../modules/home/ai.nix
+          ../../modules/home/apps.nix
+          ../../modules/home/dev.nix
+          ../../modules/home/tui.nix
+          ../../modules/home/programs/fish.nix
+          ../../modules/home/programs/neovim.nix
+          ../../modules/home/programs/direnv.nix
+        ];
+        home.stateVersion = "24.05";
+      };
+  };
+}
